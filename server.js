833949749d9e73527c58d620cec0f39a78610a80
Cliff <fabricatedmind@gmail.com>
2016-12-28 16:34:20 +0000

    microservice complete

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+//var useragent = require('express-useragent');
+
+//app.use(useragent.express());
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendStatus(200);
+});
+
+app.get("/", function(req, res){
+   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+   var language = req.acceptsLanguages();
+   var os = req.headers['user-agent'];
+   var regExp = /\(([^)]+)\)/;
+   var matches = regExp.exec(os);
+   var results = {
+       ipaddress: ip,
+       language: language[0],
+       software: matches[1]
+   }
+   console.log(JSON.stringify(results));
+    res.send(JSON.stringify(results));
+});
+app.listen(8080, function(){
+   console.log("Starting"); 
+}); 
